f52025d614b89482dd28c23033a75a6e03c542db
diana.drajneanu <drajneanu_diana@yahoo.com>
2020-03-03 12:44:46 +0200

    error handler Json type

--- lib/ValueAnonymizer/Json.php
+++ lib/ValueAnonymizer/Json.php
@@ -14,7 +14,6 @@ use PayU\MysqlDumpAnonymizer\Helper\EscapeString;
 class Json implements ValueAnonymizerInterface
 {
 
-    //TODO use JSON_THROW
     public function anonymize(Value $value, array $row, Config $config): AnonymizedValue
     {
         if ($value->isExpression()) {
@@ -24,15 +23,20 @@ class Json implements ValueAnonymizerInterface
         $jsonString = str_replace(["\r", "\n"], ["\\r", "\\n"], $value->getUnEscapedValue());
 
         try {
-            $array = json_decode($jsonString, true, 512);
+            $array = json_decode($jsonString, true, 512, JSON_THROW_ON_ERROR);
 
             if (is_array($array)) {
-                return new AnonymizedValue(EscapeString::escape(json_encode($this->anonymizeArray($array, $config))));
+                return new AnonymizedValue(EscapeString::escape(
+                    json_encode($this->anonymizeArray($array, $config), JSON_THROW_ON_ERROR, 512)
+                ));
             }
-        } catch (JsonException $e) {
-        }
 
-        return (new FreeText())->anonymize($value, $row, $config);
+            return (new FreeText())->anonymize($value, $row, $config);
+
+        } catch (JsonException $e) {
+
+            return (new FreeText())->anonymize($value, $row, $config);
+        }
     }
 
     private function anonymizeArray(array $array, Config $config): array
